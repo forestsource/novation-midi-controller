be17b438b7947394c9740a2293d9f4076abaf32e
forestsource <forestsources@gmail.com>
2022-07-12 16:31:08 +0900

    Add logger

--- launchpad.py
+++ launchpad.py
@@ -5,6 +5,8 @@ import csv
 import itertools
 from logging import getLogger
 
+import coloredlogs
+
 COLORS = {
     "black": 0, #000000
     "dark_gray": 1,#1C1C1C
@@ -20,7 +22,6 @@ COLORS = {
     "yellow_sun": 12, #FDFD21
     "dark_bronze": 13, #585800
     "smokey_black": 14, #181800
-
 }
 
 class LaunchPadProMk2:
@@ -42,6 +43,7 @@ class LaunchPadProMk2:
 
     def __init__(self, mode="STANDALONE"):
         self.logger = getLogger(__name__)
+        coloredlogs.install(level='INFO', logger=self.logger)
         midi_outputs = mido.get_output_names()
         midi_inputs = mido.get_input_names()
         self.MODE[mode]
@@ -50,16 +52,16 @@ class LaunchPadProMk2:
 
         # validation
         if len(input_port) >= 2:
-            print("Too many Launchpad Pro(input) exists. Only support 1")
+            self.logger.critical("Too many Launchpad Pro(input) exists. Only support 1")
             sys.exit()
         if len(output_port) >= 2:
-            print("Too many Launchpad Pro(output) exists. Only support 1")
+            self.logger.critical("Too many Launchpad Pro(output) exists. Only support 1")
             sys.exit()
         if not len(input_port) == 1:
-            print("Launchpad Pro(input)is not exist.")
+            self.logger.critical("Launchpad Pro(input)is not exist.")
             sys.exit()
         if not len(output_port) == 1:
-            print("Launchpad Pro(output) is not exist.")
+            self.logger.critical("Launchpad Pro(output) is not exist.")
             sys.exit()
         self._input_port_name = input_port[0]
         self._output_port_name = output_port[0]
@@ -88,7 +90,7 @@ class LaunchPadProMk2:
 
     def column_lit(self, column, color=[1]):
         if len(color) > 10:
-            print(" *Warning* Column max is 10")
+            self.logger.warn("Column max is 10")
             color = color[0:9]
 
         msg = mido.Message('sysex', data=[0, 32, 41, 2, 16, 12, column])
@@ -98,7 +100,7 @@ class LaunchPadProMk2:
 
     def row_lit(self, column, color=[1]):
         if len(color) > 10:
-            print(" *Warning* Row max is 10")
+            self.logger.warn("Row max is 10")
             color = color[0:9]
 
         msg = mido.Message('sysex', data=[0, 32, 41, 2, 16, 12, column])
@@ -114,7 +116,7 @@ class LaunchPadProMk2:
 
     def set_lit(self, colors=[1]):
         if len(colors) >= 97:
-            print(f"current color length is {len(colors)}. Max color length is 97")
+            self.logger.warn(f"current color length is {len(colors)}. Max color length is 97")
             colors = colors[:96]
         msg = mido.Message('sysex', data=[0, 32, 41, 2, 16, 14])
         msg.data += colors
